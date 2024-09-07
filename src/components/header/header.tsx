95ec6dceefcb6015c20d4de06b5c5cac8bc81ec4
Ulugbek <ulugb3k270@gmail.com>
2024-09-07 18:51:34 +0500

    feat: implemented headr

---
+++ src/components/header/header.tsx
@@ -0,0 +1,15 @@
+import { SearchInput } from "../search-input";
+import { HeaderLinks } from "./header-links";
+import { Link } from "react-router-dom";
+
+export const Header = () => {
+  return (
+    <header className="flex items-center justify-between container py-3">
+      <Link to="/">
+        <img src="/icons/logo.svg" alt="logo" className="w-[215px] h-[32px]" />
+      </Link>
+      <SearchInput />
+      <HeaderLinks />
+    </header>
+  );
+};
